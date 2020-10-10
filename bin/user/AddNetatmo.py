6d507200136e0275d10426860e93d0e0e9a00f05
Drealine <61891683+Drealine@users.noreply.github.com>
2020-10-10 15:12:40 +0200

    Add rainfall_daily debug log

--- bin/user/AddNetatmo.py
+++ bin/user/AddNetatmo.py
@@ -166,6 +166,7 @@ class NetatmoAPI():
     def calculate_rain(self, dt, rainfall_daily):
 
         rain = None
+        logdbg("Rainfall_Daily set : {}".format(self.current_rain))
 
         if dt is not None and self.last_midnight < dt:
             loginf('Reset rainfall_Daily at midnight')
